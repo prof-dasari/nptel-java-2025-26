6b3a4bb0025b52079f29e46287ec2ecef83150e6
Dasari <sree91@gmail.com>
2026-02-27 12:59:11 +0530

    Create CheckedException.java

---
+++ Exception Handling/CheckedException.java
@@ -0,0 +1,19 @@
+package checkedexception;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CheckedException {
+
+    public static void main(String[] args) {
+        try {
+            File file = new File("nonexistent.txt");
+            FileReader reader = new FileReader(file);
+        } catch (IOException e) {
+            System.out.println("Caught IOException: " + e.getMessage());
+        }
+    }
+}
+
+
