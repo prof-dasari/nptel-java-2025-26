2110a2d823a61050a9b1dfcd16c4737eccd1ee05
prof-dasari <100552385+prof-dasari@users.noreply.github.com>
2026-02-27 15:42:34 +0530

    Add files via upload

---
+++ Exception Handling/TryWithResources.java
@@ -0,0 +1,30 @@
+package trywithresources;
+
+import java.io.*;
+import java.util.*;
+
+public class TryWithResources {
+
+    static void Divide() throws Exception {
+
+        try (FileInputStream fi = new FileInputStream("C:\\Users\\LENOVO\\Documents\\NetBeansProjects\\TryWithResources\\src\\trywithresources\\Test.txt"); Scanner sc = new Scanner(fi)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println(a / c);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Divide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        //int x=sc.nextInt();
+        // System.out.println(x);
+    }
+
+}
